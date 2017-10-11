8cec561a92ae3675ae6a6a0ff932860f29a43a78
Neil Fuller <nfuller@google.com>
2017-10-11 10:38:17 +0100

    Generate the tzlookup.xml file from a proto file
    This commit adds a platform developer command-line
    tool to generate the tzlookup.xml from a human-readable
    .proto file rather than just copying the .xml file
    around.
    
    The only files that influence on-device behavior
    are those in system/timezone/output_data.
    
    The new countryzones.txt is a translation of the
    old tzlookup.xml with more structure and some
    additional data. A few offsets were found to be
    incorrect by the new validation code and these have
    been corrected (this information was previously found
    in comments in tzlookup.xml and was never actually used
    on device).
    
    During generation the tzlookup_generator can validate
    the input against IANA and ICU data and do some
    defaulting which should help avoid the data becoming
    out of date.
    
    This change introduces a country-level attribute to the
    tzlookup.xml for the "country default" time zone, which is
    there to help address bug 34266809 in a follow-up
    change.
    
    This change also moves the zone_compactor tool to
    being built with the Android build system for consistency.
    
    The change updates the update-tzdata.py script to use the
    new tool.
    
    The updated output_data files from running the tool are
    included in this commit.
    
    The new tool has tests instructions for execution are
    in README.android.
    
    Bug: 34266809
    Bug: 65575839
    Test: Booted device
    Test: See included host tests.
    Change-Id: Ic96af7987c61690faebbc80907209f9e1cc1e976
    Merged-In: Ic96af7987c61690faebbc80907209f9e1cc1e976
    (cherry picked from commit 2f3a32b91c23523450c3d1abde303447e9a6d17b)

---
+++ tzlookup_generator/src/main/java/com/android/libcore/timezone/tzlookup/TzLookupGenerator.java
@@ -0,0 +1,294 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.libcore.timezone.tzlookup;
+
+import com.android.libcore.timezone.tzlookup.proto.CountryZonesFile;
+import com.ibm.icu.util.TimeZone;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * Generates the tzlookup.xml file using the information from countryzones.txt and zones.tab.
+ *
+ * See {@link #main(String[])} for commandline information.
+ */
+public final class TzLookupGenerator {
+
+    private final String countryZonesFile;
+    private final String zoneTabFile;
+    private final String outputFile;
+
+    /**
+     * Executes the generator.
+     *
+     * Positional arguments:
+     * 1: The countryzones.txt file
+     * 2: the zone.tab file
+     * 3: the file to generate
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println(
+                    "usage: java com.android.libcore.timezone.tzlookup.proto.TzLookupGenerator"
+                            + " <input proto file> <zone.tab file> <output xml file>");
+            System.exit(0);
+        }
+        boolean success = new TzLookupGenerator(args[0], args[1], args[2]).execute();
+        System.exit(success ? 0 : 1);
+    }
+
+    TzLookupGenerator(String countryZonesFile, String zoneTabFile, String outputFile) {
+        this.countryZonesFile = countryZonesFile;
+        this.zoneTabFile = zoneTabFile;
+        this.outputFile = outputFile;
+    }
+
+    boolean execute() throws IOException {
+        // Parse the countryzones input file.
+        CountryZonesFile.CountryZones countryZonesIn;
+        try {
+            countryZonesIn = CountryZonesFileSupport.parseCountryZonesTextFile(countryZonesFile);
+        } catch (ParseException e) {
+            logError("Unable to parse " + countryZonesFile, e);
+            return false;
+        }
+
+        // Check the countryzones rules version matches the version that ICU is using.
+        String icuTzDataVersion = TimeZone.getTZDataVersion();
+        String inputIanaVersion = countryZonesIn.getIanaVersion();
+        if (!icuTzDataVersion.equals(inputIanaVersion)) {
+            logError("Input data is for " + inputIanaVersion + " but the ICU you have is for "
+                    + icuTzDataVersion);
+            return false;
+        }
+
+        // Pull out information we want to validate against from zone.tab (which we have to assume
+        // matches the ICU version since it doesn't contain its own version info).
+        ZoneTabFile zoneTabIn = ZoneTabFile.parse(zoneTabFile);
+        Map<String, List<String>> zoneTabMapping =
+                ZoneTabFile.createCountryToOlsonIdsMap(zoneTabIn);
+        List<CountryZonesFile.Country> countriesIn = countryZonesIn.getCountriesList();
+        List<String> countriesInIsos = CountryZonesFileSupport.extractIsoCodes(countriesIn);
+
+        // Sanity check the countryzones file only contains lower-case country codes. The output
+        // file uses them and the on-device code assumes lower case.
+        if (!Utils.allLowerCaseAscii(countriesInIsos)) {
+            logError("Non-lowercase country ISO codes found in: " + countriesInIsos);
+            return false;
+        }
+        // Sanity check the countryzones file doesn't contain duplicate country entries.
+        if (!Utils.allUnique(countriesInIsos)) {
+            logError("Duplicate input country entries found: " + countriesInIsos);
+            return false;
+        }
+
+        // Validate the country iso codes found in the countryzones against those in zone.tab.
+        // zone.tab uses upper case, countryzones uses lower case.
+        List<String> upperCaseCountriesInIsos = Utils.toUpperCase(countriesInIsos);
+        Set<String> timezonesCountryIsos = new HashSet<>(upperCaseCountriesInIsos);
+        Set<String> zoneTabCountryIsos = zoneTabMapping.keySet();
+        if (!zoneTabCountryIsos.equals(timezonesCountryIsos)) {
+            logError(zoneTabFile + " contains "
+                    + Utils.subtract(zoneTabCountryIsos, timezonesCountryIsos)
+                    + " not present in countryzones, "
+                    + countryZonesFile + " contains "
+                    + Utils.subtract(timezonesCountryIsos, zoneTabCountryIsos)
+                    + " not present in zonetab.");
+            return false;
+        }
+
+        // Start constructing the output structure.
+        TzLookupFile.TimeZones timeZonesOut = new TzLookupFile.TimeZones(inputIanaVersion);
+        TzLookupFile.CountryZones countryZonesOut = new TzLookupFile.CountryZones();
+        timeZonesOut.setCountryZones(countryZonesOut);
+
+        Errors processingErrors = new Errors();
+
+        // Process each Country.
+        for (CountryZonesFile.Country countryIn : countriesIn) {
+            String isoCode = countryIn.getIsoCode();
+            processingErrors.pushScope("country=" + isoCode);
+            try {
+                // Each Country must have >= 1 time zone.
+                List<CountryZonesFile.TimeZone> timeZonesIn = countryIn.getTimeZonesList();
+                if (timeZonesIn.isEmpty()) {
+                    processingErrors.addFatal("No time zones");
+                    continue;
+                }
+
+                // Look for duplicate time zone IDs.
+                List<String> countryTimeZoneIds = CountryZonesFileSupport.extractIds(timeZonesIn);
+                if (!Utils.allUnique(countryTimeZoneIds)) {
+                    processingErrors.addFatal("country's zones=" + countryTimeZoneIds
+                            + " contains duplicates");
+                }
+
+                // Each Country needs a default time zone ID (but we can guess in some cases).
+                String defaultTimeZoneId;
+                if (countryIn.hasDefaultTimeZoneId()) {
+                    defaultTimeZoneId = countryIn.getDefaultTimeZoneId();
+                    if (!validTimeZoneId(defaultTimeZoneId)) {
+                        processingErrors.addFatal(
+                                "Default time zone ID " + defaultTimeZoneId + " is not valid");
+                        continue;
+                    }
+                } else {
+                    if (timeZonesIn.size() > 1) {
+                        processingErrors.addFatal(
+                                "To pick a default time zone there must be a single offset group");
+                        continue;
+                    }
+                    defaultTimeZoneId = timeZonesIn.get(0).getId();
+                }
+
+                // Validate the default.
+                if (!countryTimeZoneIds.contains(defaultTimeZoneId)) {
+                    processingErrors.addFatal("defaultTimeZoneId=" + defaultTimeZoneId
+                            + " is not one of the country's zones=" + countryTimeZoneIds);
+                }
+
+                // Add the country to the output structure.
+                TzLookupFile.Country countryOut =
+                        new TzLookupFile.Country(isoCode, defaultTimeZoneId);
+                countryZonesOut.addCountry(countryOut);
+
+                // Validate the country information against the equivalent information in zone.tab.
+                processingErrors.pushScope("zone.tab comparison");
+                try {
+                    List<String> zoneTabCountryTimeZoneIds =
+                            zoneTabMapping.get(isoCode.toUpperCase());
+                    if (zoneTabCountryTimeZoneIds == null) {
+                        processingErrors.addFatal("Unknown country=" + isoCode);
+                        continue;
+                    }
+
+                    // Look for unexpected duplicate time zone IDs in zone.tab
+                    if (!Utils.allUnique(zoneTabCountryTimeZoneIds)) {
+                        processingErrors.addFatal(
+                                "Duplicate time zone IDs found:" + zoneTabCountryTimeZoneIds);
+                    }
+
+                    if (!Utils.setEquals(zoneTabCountryTimeZoneIds, countryTimeZoneIds)) {
+                        processingErrors.addFatal("IANA lists " + isoCode
+                                + " as having zones: " + zoneTabCountryTimeZoneIds
+                                + ", but countryzones has " + countryTimeZoneIds);
+                        continue;
+                    }
+                } finally {
+                    processingErrors.popScope();
+                }
+
+                // Process each input time zone.
+                for (CountryZonesFile.TimeZone timeZoneIn : timeZonesIn) {
+                    processingErrors.pushScope(
+                            "offset=" + timeZoneIn.getUtcOffset() + ", id=" + timeZoneIn.getId());
+                    try {
+                        // Validate the offset information in countryzones.
+                        validateNonDstOffset(countryIn, timeZoneIn, processingErrors);
+
+                        String timeZoneInId = timeZoneIn.getId();
+                        // Add the id to the output structure.
+                        TzLookupFile.TimeZoneIdentifier timeZoneIdOut =
+                                new TzLookupFile.TimeZoneIdentifier(timeZoneInId);
+                        countryOut.addTimeZoneIdentifier(timeZoneIdOut);
+                    } finally {
+                        processingErrors.popScope();
+                    }
+                }
+            } finally{
+                // End of country processing.
+                processingErrors.popScope();
+            }
+        }
+
+        if (!processingErrors.isFatal()) {
+            // Write the output structure if there wasn't a fatal error.
+            logInfo("Writing " + outputFile);
+            try {
+                TzLookupFile.write(timeZonesOut, outputFile);
+            } catch (XMLStreamException e) {
+                e.printStackTrace(System.err);
+                processingErrors.addFatal("Unable to write output file");
+            }
+        }
+
+        // Report all warnings / errors
+        if (!processingErrors.isEmpty()) {
+            logInfo("Issues:\n" + processingErrors.asString());
+        }
+
+        return !processingErrors.isFatal();
+    }
+
+    private static boolean validTimeZoneId(String timeZoneId) {
+        TimeZone zone = TimeZone.getTimeZone(timeZoneId);
+        return !zone.getID().equals(TimeZone.UNKNOWN_ZONE_ID);
+    }
+
+    private static void validateNonDstOffset(
+            CountryZonesFile.Country country, CountryZonesFile.TimeZone timeZoneIn, Errors errors) {
+        String utcOffsetString = timeZoneIn.getUtcOffset();
+        long utcOffsetMillis;
+        try {
+            utcOffsetMillis = Utils.parseUtcOffsetToMillis(utcOffsetString);
+        } catch (ParseException e) {
+            errors.addFatal("Bad offset string: " + utcOffsetString);
+            return;
+        }
+
+        final long minimumGranularity = TimeUnit.MINUTES.toMillis(15);
+        if (utcOffsetMillis % minimumGranularity != 0) {
+            errors.addWarning(
+                    "Unexpected granularity: not a multiple of 15 minutes: " + utcOffsetString);
+        }
+
+        String timeZoneIdIn = timeZoneIn.getId();
+        if (!validTimeZoneId(timeZoneIdIn)) {
+            errors.addFatal("Time zone ID=" + timeZoneIdIn + " is not valid");
+            return;
+        }
+
+        // Check the offset Android has matches what ICU thinks.
+        TimeZone timeZone = TimeZone.getTimeZone(timeZoneIdIn);
+        int actualOffsetMillis = timeZone.getRawOffset();
+        if (actualOffsetMillis != utcOffsetMillis) {
+            errors.addFatal("Offset mismatch: You will want to confirm the ordering for "
+                    + country.getIsoCode() + " still makes sense. Raw offset for "
+                    + timeZoneIdIn + " is " + Utils.toUtcOffsetString(actualOffsetMillis)
+                    + " and not " + Utils.toUtcOffsetString(utcOffsetMillis));
+        }
+    }
+
+    private static void logError(String msg) {
+        System.err.println("E: " + msg);
+    }
+
+    private static void logError(String s, Throwable e) {
+        logError(s);
+        e.printStackTrace(System.err);
+    }
+
+    private static void logInfo(String msg) {
+        System.err.println("I: " + msg);
+    }
+}
