bc2b46155b1444b74e3b737c9f6148fc6ec327d5
Patrice Arruda <patricearruda@google.com>
2019-05-13 09:13:07 -0700

    Convert system/timezone/output_data/version/Android.mk to Android.bp.
    Convert most of the system/timezone/output_data/version/Android.mk modules
    to Android.bp file.
    
    Bug: b/122331946
    Test: Ran build.
    Change-Id: I9648a10f6f04f5d2b0e3944e2cba6e6e52792819

--- output_data/version/Android.mk
+++ output_data/version/Android.mk
@@ -15,29 +15,10 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := tz_version
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT)/usr/share/zoneinfo
-include $(BUILD_PREBUILT)
-
 ifeq ($(WITH_HOST_DALVIK),true)
 
 # A host version of the tz_version module for use by
 # hostdex rules.
-
-# Simulate /system
-include $(CLEAR_VARS)
-LOCAL_MODULE := tz_version_host
-LOCAL_IS_HOST_MODULE := true
-LOCAL_SRC_FILES := tz_version
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := $(LOCAL_SRC_FILES)
-LOCAL_MODULE_PATH := $(HOST_OUT)/usr/share/zoneinfo
-include $(BUILD_PREBUILT)
 
 # Simulate the runtime module
 include $(CLEAR_VARS)
@@ -51,21 +32,3 @@ LOCAL_MODULE_PATH := $(HOST_OUT)/com.android.runtime/etc/tz
 include $(BUILD_PREBUILT)
 
 endif
-
-# Module definition producing a tz_version prebuilt file in
-# /system/etc/tz for standalone ART testing purposes. This is a
-# temporary change needed until the ART Buildbot and Golem both fully
-# support the Runtime APEX (see b/121117762). This module should never
-# be shipped by default (i.e. should never be part of
-# `PRODUCT_PACKAGE`.)
-#
-# TODO(b/121117762, b/129332183): Remove this module definition when
-# the ART Buildbot and Golem have full support for the Runtime APEX.
-include $(CLEAR_VARS)
-LOCAL_MODULE := tz_version-art-test
-LOCAL_MODULE_STEM := tz_version
-LOCAL_SRC_FILES := $(LOCAL_MODULE_STEM)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/tz
-include $(BUILD_PREBUILT)
