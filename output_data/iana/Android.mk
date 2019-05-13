f22126fa96b2ae8694a1a7b7cc09a06ff4d1386d
Patrice Arruda <patricearruda@google.com>
2019-05-13 10:35:06 -0700

    Convert system/timezone/output_data/iana/Android.mk to Android.bp.
    Converted most of the system/timezone/output_data/iana/Android.mk
    modules to Android.bp modules.
    
    Bug: b/122331946
    Test: Ran build.
    Change-Id: I8ef35dae6ad3eb0456795570e37acc6de545b03b

--- output_data/iana/Android.mk
+++ output_data/iana/Android.mk
@@ -15,29 +15,10 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := tzdata
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT)/usr/share/zoneinfo
-include $(BUILD_PREBUILT)
-
 ifeq ($(WITH_HOST_DALVIK),true)
 
 # A host version of the tzdata module for use by
 # hostdex rules.
-
-# Simulate /system
-include $(CLEAR_VARS)
-LOCAL_MODULE := tzdata_host
-LOCAL_IS_HOST_MODULE := true
-LOCAL_SRC_FILES := tzdata
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := $(LOCAL_SRC_FILES)
-LOCAL_MODULE_PATH := $(HOST_OUT)/usr/share/zoneinfo
-include $(BUILD_PREBUILT)
 
 # Simulate the runtime module.
 include $(CLEAR_VARS)
@@ -51,21 +32,3 @@ LOCAL_MODULE_PATH := $(HOST_OUT)/com.android.runtime/etc/tz
 include $(BUILD_PREBUILT)
 
 endif
-
-# Module definition producing a tzdata prebuilt file in
-# /system/etc/tz for standalone ART testing purposes. This is a
-# temporary change needed until the ART Buildbot and Golem both fully
-# support the Runtime APEX (see b/121117762). This module should never
-# be shipped by default (i.e. should never be part of
-# `PRODUCT_PACKAGE`.)
-#
-# TODO(b/121117762, b/129332183): Remove this module definition when
-# the ART Buildbot and Golem have full support for the Runtime APEX.
-include $(CLEAR_VARS)
-LOCAL_MODULE := tzdata-art-test
-LOCAL_MODULE_STEM := tzdata
-LOCAL_SRC_FILES := $(LOCAL_MODULE_STEM)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/tz
-include $(BUILD_PREBUILT)
